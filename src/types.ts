923d0e92b3d9c4b1ec34edb06994d5fc19ffe2a2
mattrheault-maze <matt.rheault@maze.design>
2025-03-08 20:03:52 -0500

    Support globals field, and dimension identifiers

--- src/types.ts
+++ src/types.ts
@@ -4,17 +4,44 @@ export interface AnalyticsSchemaProperty {
   type: string | string[];
 }
 
+export interface AnalyticsSchemaDimensionIdentifier {
+  property: string;
+  contains?: (string | number | boolean)[];
+  equals?: string | number | boolean;
+  not?: string | number | boolean;
+  in?: (string | number | boolean)[];
+  notIn?: (string | number | boolean)[];
+  startsWith?: string;
+  endsWith?: string;
+  lt?: number;
+  lte?: number;
+  gt?: number;
+  gte?: number;
+}
+
+export interface AnalyticsSchemaDimension {
+  name: string;
+  description: string;
+  identifiers: AnalyticsSchemaDimensionIdentifier[];
+}
+
+export interface AnalyticsSchemaGlobals {
+  dimensions: AnalyticsSchemaDimension[];
+  properties: AnalyticsSchemaProperty[];
+}
+
+export interface AnalyticsSchemaEvent {
+  name: string;
+  description: string;
+  version?: string;
+  dimensions?: string[];
+  properties?: AnalyticsSchemaProperty[];
+}
+
 export interface AnalyticsSchema {
   version: string;
-  validDimensions: string[];
-  globalProperties: AnalyticsSchemaProperty[];
-  events: {
-    name: string;
-    description: string;
-    version?: string;
-    dimensions?: string[];
-    properties?: AnalyticsSchemaProperty[];
-  }[];
+  globals: AnalyticsSchemaGlobals; // Moved validDimensions & globalProperties under "globals"
+  events: Record<string, AnalyticsSchemaEvent>;
 }
 
 export interface AnalyticsTracker {
