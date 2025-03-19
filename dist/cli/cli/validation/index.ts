0949c2daefef2e9324a04c0283fad9aa80a97fda
mattrheault-maze <matt.rheault@maze.design>
2025-03-18 20:26:00 -0400

    fix double copying of cli folder on build

--- dist/cli/cli/validation/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./types";
-export * from "./fileValidation";
-export * from "./schemaValidation";
-export * from "./logging";
-export { validateAnalyticsFiles } from "./validateAnalyticsFiles";
-export { validateGlobals, defaultGlobals } from "./validateAnalyticsGlobals";
-export { validateEvents } from "./validateAnalyticsEvents";
-export { validateAnalyticsConfig } from "./validateAnalyticsConfig"; 
--- dist/cli/cli/validation/schemaValidation.ts
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-import Ajv from "ajv";
-
-export function createValidator(schemaPath: string) {
-  const ajv = new Ajv();
-  const schema = JSON.parse(fs.readFileSync(schemaPath, "utf8"));
-  return ajv.compile(schema);
-} 
