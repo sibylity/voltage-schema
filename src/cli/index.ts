427073275c360de9136fe41d3a074828ebf1e13b
mattrheault-maze <matt.rheault@maze.design>
2025-03-28 16:32:23 -0400

    Add a properties CLI command

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,12 +2,14 @@ import { program } from "commander";
 import { registerInitCommand } from "./commands/init";
 import { registerValidateCommand } from "./commands/validate";
 import { registerDimensionsCommand } from "./commands/dimensions";
+import { registerPropertiesCommand } from "./commands/properties";
 import { registerGenerateCommand } from "./commands/generate";
 
 // Register all commands
 registerInitCommand(program);
 registerValidateCommand(program);
 registerDimensionsCommand(program);
+registerPropertiesCommand(program);
 registerGenerateCommand(program);
 
 // Parse command line arguments
