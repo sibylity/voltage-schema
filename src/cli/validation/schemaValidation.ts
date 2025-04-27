d5dbbe51b2d00d4c01634fc93bce5f1054c37088
mattrheault-maze <matt.rheault@maze.design>
2025-04-27 13:45:52 -0400

    Disable strict mode to silence warnings

--- src/cli/validation/schemaValidation.ts
+++ src/cli/validation/schemaValidation.ts
@@ -2,7 +2,9 @@ import fs from "fs";
 import Ajv from "ajv";
 
 export function createValidator(schemaPath: string) {
-  const ajv = new Ajv();
+  const ajv = new Ajv({
+    allowUnionTypes: true // Allow union types to suppress strict mode warnings
+  });
   const schema = JSON.parse(fs.readFileSync(schemaPath, "utf8"));
   return ajv.compile(schema);
 } 
